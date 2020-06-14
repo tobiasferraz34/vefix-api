1e5f3b17f79c6ce0bef793baa35bba2a33db25a4
tobiasferraz34 <tobiasferraz34@gmail.com>
2020-06-14 14:37:25 -0300

    Pasta node_modules removida

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const conexao = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'teste'
+    host: 'db4free.net',
+    user: 'vefix_atend',
+    password: 'ubuntu040696',
+    database: 'vefix_teste'
 });
 
 module.exports = conexao
