515df0a67221e77dc05296647cce71a8ab38adde
tobiasferraz34 <tobiasferraz34@gmail.com>
2020-06-16 17:02:30 -0300

    Alteração na origin do cors

--- config/customExpress.js
+++ config/customExpress.js
@@ -5,13 +5,10 @@ const cors = require('cors');
 //FUNÇÃO RESPONSÁVEL POR CONFIGURAR O APP DO EXPRESS
 module.exports = () => {
     const app = express();
+    app.use(cors({ origin: 'http://localhost:3000'}))
+
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
-    app.use(cors({ origin: 'http://localhost:3000/', // client (todo mundo pode acessar)
-
-        optionsSuccessStatus: 200
-        
-        }))
 
     consign()
         .include('controllers')
