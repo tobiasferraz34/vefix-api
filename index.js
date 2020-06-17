606b1a7dfbdb2bf5475255f7a44525b0d7cc2e30
tobiasferraz34 <tobiasferraz34@gmail.com>
2020-06-17 12:52:34 -0300

    'TESTE'

--- index.js
+++ index.js
@@ -3,17 +3,9 @@ const customExpress = require('./config/customExpress');
 const conexao = require('./infraestrutura/conexao');
 const Tabelas = require('./infraestrutura/tabelas');
 
-conexao.connect(erro => {
-    if (erro) {
-        console.log(erro);
-    } else {
-        console.log("Conectado com sucesso");
-
-        Tabelas.init(conexao);
-        const app = customExpress();
-        var port = process.env.PORT || 3000;
-        app.listen(port, () => console.log("Servidor rodando na porta 4000"));
-    }
-})
+Tabelas.init(conexao);
+const app = customExpress();
+var port = 3000;
+app.listen(port, () => console.log("Servidor rodando na porta 3000"));
 
 
--- infraestrutura/backup-conexao/desenvolvimento/conexao.js
+++ infraestrutura/backup-conexao/desenvolvimento/conexao.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
 const conexao = mysql.createPool({
-    host: 'mysql669.umbler.com',
-    user: 'vefix',
-    password: 'vefix6287',
+    host: 'localhost',
+    user: 'root',
+    password: '',
     database: 'vefix'
 });
 
