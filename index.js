3eeaea7af2b4d394825db91ce0f3eea10a58759d
tobias ferraz <tobiasferraz34@gmail.com>
2021-08-23 23:35:08 -0300

    teste

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const customExpress = require('./config/customExpress');
 const conexao = require('./infraestrutura/conexao');
 const Tabelas = require('./infraestrutura/tabelas');
 
-Tabelas.init(conexao);
+//Tabelas.init(conexao);
 const app = customExpress();
 var port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Servidor rodando na porta 4000"));
