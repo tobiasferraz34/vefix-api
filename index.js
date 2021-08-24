19cd08a35ed8bf9cafc35eb4e5f83d46482c8dc7
tobias ferraz <tobiasferraz34@gmail.com>
2021-08-24 16:39:18 -0300

    Atualizando .env

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ require("dotenv-safe").config();
 //A responsabilidade desse arquivo é subir o servidor
 const customExpress = require('./config/customExpress');
 const conexao = require('./infraestrutura/conexao');
-const Tabelas = require('./infraestrutura/tabelas');
+//const Tabelas = require('./infraestrutura/tabelas');
 
-Tabelas.init(conexao);
+//Tabelas.init(conexao);
 const app = customExpress();
 var port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Servidor rodando na porta 4000"));
